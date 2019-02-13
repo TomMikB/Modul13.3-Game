a4330f6469a3413008201bf27ea31d09238a1043
Tomasz Biernacki <tmbiernacki@szczecin.home.pl>
2019-02-13 19:34:02 +0100

    Stage 3

--- js/index.js
+++ js/index.js
@@ -2,19 +2,21 @@
 
 (function(){
   
-  var buttonPaper = document.getElementById('paperButton');
-  var buttonRock = document.getElementById('rockButton');
-  var buttonScissors = document.getElementById('scissorsButton');
-  var howManyRounds = document.getElementById('startGame');
-  var roundsMessage = document.getElementById('roundsNo');
-  var gameScore = document.getElementById('result');
-  var message = document.getElementById('output');
-  var playerScore = 0;
-  var computerScore = 0;
-  var noOfRounds = 0;
-  var startGame = false;
+  var params = {
+    howManyRounds: document.getElementById('startGame'), 
+    roundsMessage: document.getElementById('roundsNo'), 
+    gameScore: document.getElementById('result'), 
+    message: document.getElementById('output'), 
+    playerScore: 0, 
+    computerScore: 0, 
+    noOfRounds: 0, 
+    startGame: false
+  };
+  var playerChoice = document.querySelectorAll('.player-move');
+
+//  var abc = document.getElementById('testtxt'); // do testów
   
-  message.innerHTML = 'Welcome! Press NEW GAME to start game.';
+  params.message.innerHTML = 'Welcome! Press NEW GAME to start game.';
   
 /*computer move*/
   var computerMoveGenerator = function () {
@@ -36,23 +38,23 @@
       (computerMove === 'ROCK' && playerMove === 'SCISSORS') ||
       (computerMove === 'SCISSORS' && playerMove === 'PAPER')
     ) {
-      computerScore++;
+      params.computerScore++;
       return 'YOU LOST:';
     }
-    playerScore++;
+    params.playerScore++;
     return 'YOU WON:';
   };
   
 /*win message*/
   var printResult = function (playerMove, computerMove, playerRoundResult) {
-    message.innerHTML = playerRoundResult + ' you played ' + playerMove + ', computer played ' + computerMove + '.';
-    gameScore.innerHTML = playerScore + ' - ' + computerScore;
-    if (playerScore == noOfRounds) {
-      startGame = false;
-      message.innerHTML += '<br> CONGRATULATIONS! YOU WON THE ENTIRE GAME!!!';
-    } else if (computerScore == noOfRounds) {
-      startGame = false;
-      message.innerHTML += '<br> SORRY! YOU LOST THE ENTIRE GAME!!!';
+    params.message.innerHTML = playerRoundResult + ' you played ' + playerMove + ', computer played ' + computerMove + '.';
+    params.gameScore.innerHTML = params.playerScore + ' - ' + params.computerScore;
+    if (params.playerScore == params.noOfRounds) {
+      params.startGame = false;
+      params.message.innerHTML += '<br> CONGRATULATIONS! YOU WON THE ENTIRE GAME!!!';
+    } else if (params.computerScore == params.noOfRounds) {
+      params.startGame = false;
+      params.message.innerHTML += '<br> SORRY! YOU LOST THE ENTIRE GAME!!!';
     }
   };
 
@@ -66,50 +68,35 @@
 /*game not started*/
   var notStarted = function () {
     var startMessage = '<br>GAME OVER! Please press the NEW GAME button!'
-    if (message.innerHTML.indexOf('GAME OVER') === -1) {
-      message.innerHTML += startMessage;
+    if (params.message.innerHTML.indexOf('GAME OVER') === -1) {
+      params.message.innerHTML += startMessage;
     };
   };
   
-/*player play paper*/
-  buttonPaper.addEventListener('click', function() {
-    if (startGame) {
-      playerDecision('PAPER');
-    } else if (message.innerHTML.indexOf('Welcome') === -1) {
-      notStarted();
-    }
-  });
-  
-/*player play rock*/
-  buttonRock.addEventListener('click', function() {
-    if (startGame) {
-      playerDecision('ROCK');
-    } else if (message.innerHTML.indexOf('Welcome') === -1) {
-      notStarted();
-    }
-  });
-  
-/*player play scissors*/
-  buttonScissors.addEventListener('click', function() {
-    if (startGame) {
-      playerDecision('SCISSORS');
-    } else if (message.innerHTML.indexOf('Welcome') === -1) {
-      notStarted();
-    }
-  });
+/*player play*/
+  for(var i = 0; i < playerChoice.length; i++){
+    playerChoice[i].addEventListener('click', function(event) {
+      if (params.startGame) {
+        playerDecision(event.target.getAttribute('data-move'));
+      } else if (params.message.innerHTML.indexOf('Welcome') === -1) {
+        notStarted();
+      }
+    });
+  };
+
   
 /*number of rounds*/
-  howManyRounds.addEventListener('click', function() {
-    playerScore = 0;
-    computerScore = 0;
-    noOfRounds = window.prompt('How many rounds to win?');
-    if (noOfRounds == parseInt(noOfRounds, 10) && noOfRounds > 0) {
-      startGame = true;
-      roundsMessage.innerHTML = 'Rounds to win game: ' + noOfRounds;
-      gameScore.innerHTML = playerScore + ' - ' + computerScore;
+  params.howManyRounds.addEventListener('click', function() {
+    params.playerScore = 0;
+    params.computerScore = 0;
+    params.noOfRounds = window.prompt('How many rounds to win?');
+    if (params.noOfRounds == parseInt(params.noOfRounds, 10) && params.noOfRounds > 0) {
+      params.startGame = true;
+      params.roundsMessage.innerHTML = 'Rounds to win game: ' + params.noOfRounds;
+      params.gameScore.innerHTML = params.playerScore + ' - ' + params.computerScore;
     } else {
       alert('Please use natural number (greater than 0)!');
-      startGame = false;
+      params.startGame = false;
     }
   });
 
